f15fb3a14f2477339924249a630ec880f7c60cf4
Tushar Sharma <sharmatushar811@gmail.com>
2025-09-26 17:59:08 +0530

    Cors fix

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -14,11 +14,13 @@ class AuthController {
     res.cookie("token", accesstoken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
       maxAge: 15 * 60 * 1000,
     });
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   }
